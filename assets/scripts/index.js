128a5d0e8d27168ca813f451a05e95467553c3ef
capg84 <cyprian.gomes@live.co.uk>
2023-01-14 11:12:46 +0000

    initial commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+  var menu = document.getElementById("navMenu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
